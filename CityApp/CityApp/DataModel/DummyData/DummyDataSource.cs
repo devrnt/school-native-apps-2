0ed9cc75cd762febc71a7e67d12f20b622b04a78
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-11-28 14:06:12 +0100

    Add promotions to company details

---
+++ CityApp/CityApp/DataModel/DummyData/DummyDataSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityApp.DataModel
+{
+    internal static class DummyDataSource
+    {
+        public static List<Company> Companies { get; set; } = InitializeData();
+
+        private static List<Company> InitializeData()
+        {
+            var company = new Company(1,
+                "Swashy", "Hier kunt u terrecht met al uw vuile was. Op 15min. kunt u terug keren met een vers gewassen was. Was u er vorige keer niet bij, kom dan nu zeker langs met uw was.",
+                new List<string>() { "was", "laundry", "Swashy" },
+                Categories.Wasserij,
+                new Owner("van Gestel", "Gerry", new DateTime(1995, 3, 5), "GerryvanGestel@teleworm.be", "gerrygerry", UserType.Owner),
+                new List<Location>() { new Location("Belgium", "Gent", 9000, "Grensstraat", 12, 51.076190, 3.710630) },
+                null, null, null, null);
+
+            company.Promotions = new List<Promotion>() { new Promotion("Dit is een promotie!"), new Promotion("Nog een promotie zeg") };
+
+            return new List<Company>() {
+                company,
+                new Company(2, "De Wasserte", "Beschrijving comp2", null, Categories.Wasserij, null, null, null, null, null, null),
+                new Company(3, "comp1", "Beschrijving comp1", null, Categories.Bank, null, null, null, null, null, null),
+                new Company(4, "comp2", "Beschrijving comp2", null, Categories.Bank, null, null, null, null, null, null),
+                new Company(5, "comp1", "Beschrijving comp1", null, Categories.Bank, null, null, null, null, null, null)
+            };
+        }
+    }
+}
--- CityApp/CityApp/DataModel/Promotion.cs
+++ CityApp/CityApp/DataModel/Promotion.cs
@@ -2,9 +2,16 @@
 {
     public class Promotion
     {
-        public Promotion()
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Promotion(string desription)
         {
+            Description = desription;
+        }
 
+        public override string ToString()
+        {
+            return string.Format("{0}", Description);
         }
     }
 }
