ffd5365c0c00127b956c28a23d05f17e4a2a18d0
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-12-04 12:42:48 +0100

    Add DiscountCoupon model

---
+++ CityApp/CityApp/DataModel/DiscountCoupon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityApp.DataModel
+{
+    public class DiscountCoupon
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public string Pdf { get; set; }
+
+        public DiscountCoupon(string couponCode, string pdf = "")
+        {
+            CouponCode = couponCode;
+            Pdf = pdf;
+        }
+
+        // function to format the couponCode
+        public string Print()
+        {
+            return $"De kortingscode {CouponCode}";
+        }
+    }
+}
