bba062c282c4d2fcc49ab6fff81380ef0512a597
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-12-07 15:42:44 +0100

    Add SaveChanges to Delete route of Companies

--- CityAppREST/CityAppREST/Controllers/CompaniesController.cs
+++ CityAppREST/CityAppREST/Controllers/CompaniesController.cs
@@ -70,6 +70,7 @@ namespace CityAppREST.Controllers
             }
 
             _companyRepository.Delete(id);
+            _companyRepository.SaveChanges();
 
             return toDelete;
         }
