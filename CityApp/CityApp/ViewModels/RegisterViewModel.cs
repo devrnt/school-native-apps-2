2403d8677c41dd085eab08fcefb88f8db97dda3f
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-12-09 20:26:21 +0100

    Add registration

--- CityApp/CityApp/ViewModels/RegisterViewModel.cs
+++ CityApp/CityApp/ViewModels/RegisterViewModel.cs
@@ -7,32 +7,46 @@ using CityApp.DataModel;
 using CityApp.Helpers;
 using CityApp.Services;
 using CityApp.Services.Navigation;
+using CityApp.Services.Rest;
 
 namespace CityApp.ViewModels
 {
     public class RegisterViewModel
     {
         private INavigationService _navigationService;
+        private UserService _userService;
         public RelayCommand RegisterCommand { get; set; }
+
 
         public RegisterViewModel()
         {
-            RegisterCommand = new RelayCommand((p) => Register((RegisterCredentials)p));
+            RegisterCommand = new RelayCommand((p) => RegisterAsync((RegisterCredentials)p));
             _navigationService = NavigationService.ns;
+            _userService = new UserService();
         }
 
-        private void Register(RegisterCredentials p)
+        private async void RegisterAsync(RegisterCredentials p)
         {
             UserType ut;
-            if (p.Type != null || p.Type == true) {
+            if (p.Type != null || p.Type == true)
+            {
                 ut = UserType.Visitor;
-            }else
+            }
+            else
             {
                 ut = UserType.Owner;
             }
 
+            var user = new Visitor(p.Name, p.FirstName, p.BirthDate.DateTime, p.Email, p.Password, ut, new List<Company>());
 
-            User user = new Visitor(p.Name, p.FirstName, p.BirthDate.DateTime, p.Email, p.Password, ut, new List<Company>());
+            var result = await _userService.RegisterAsync(user);
+            if(result == "Succesvol aangemaakt")
+            {
+                await _navigationService.NavigateToCompaniesAsync();
+            } else
+            {
+                // Not logged in, display error message
+            }
         }
     }
 }
