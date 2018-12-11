7c823b9e303d8ec116ef41df47d9abaf94e8772a
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-12-11 13:26:11 +0100

    Add documentation to helper classes

--- CityAppREST/CityAppREST/Helpers/PasswordHasher.cs
+++ CityAppREST/CityAppREST/Helpers/PasswordHasher.cs
@@ -3,23 +3,30 @@ using System.Security.Cryptography;
 
 namespace CityAppREST.Helpers
 {
+    /// <summary>
+    /// Helper class to hash a password and validate a password with a passwordHash
+    /// </summary>
     public static class PasswordHasher
     {
         private static readonly int _saltLength = 16;
         private static readonly int _hashLength = 20;
-        private static readonly int _hashIterations = 1000;
+        private static readonly int _hashIterations = 1000; // increasing this makes the hash more secure but impacts performance
 
         public static string GetPasswordAndSaltHash(string password)
         {
+            // Generate a random salt
             var salt = new byte[_saltLength];
             new RNGCryptoServiceProvider().GetBytes(salt);
 
+            // Hashes the password
             var rfc = new Rfc2898DeriveBytes(password, salt, _hashIterations);
             var hash = rfc.GetBytes(_hashLength);
-            var hashBytes = new byte[_saltLength + _hashLength];
 
+            // Adds password hash and salt in one byte array
+            var hashBytes = new byte[_saltLength + _hashLength];
             Array.Copy(salt, 0, hashBytes, 0, _saltLength);
             Array.Copy(hash, 0, hashBytes, _saltLength, _hashLength);
+
             return Convert.ToBase64String(hashBytes);
         }
 
@@ -27,6 +34,7 @@ namespace CityAppREST.Helpers
         {
             var hashBytes = Convert.FromBase64String(passwordHash);
 
+            // Get the salt from the hashBytes
             var salt = new byte[_saltLength];
             Array.Copy(hashBytes, salt, _saltLength);
 
--- CityAppREST/CityAppREST/Helpers/TokenGenerator.cs
+++ CityAppREST/CityAppREST/Helpers/TokenGenerator.cs
@@ -8,6 +8,9 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace CityAppREST.Helpers
 {
+    /// <summary>
+    /// Helper class to generate a JWT token with Claims
+    /// </summary>
     public class TokenGenerator
     {
         private readonly byte[] _key;
@@ -17,20 +20,23 @@ namespace CityAppREST.Helpers
         public TokenGenerator()
         {
             _key = new byte[256];
-            new RNGCryptoServiceProvider().GetBytes(_key);
+            new RNGCryptoServiceProvider().GetBytes(_key); // key is randomly generated to keep secret, can also be hardcoded in a json file
         }
 
         public string GenerateTokenForUser(User user)
         {
+            // Create security key and signs credentials with it
             var ssk = new SymmetricSecurityKey(_key);
             var creds = new SigningCredentials(ssk, SecurityAlgorithms.HmacSha256Signature);
 
+
+            // Create token with Claims containing data about the user
             var token = new JwtSecurityToken(
                 claims: new[] {
                     new Claim(ClaimTypes.Name, user.Username),
-                    new Claim(ClaimTypes.Role, user.UserType.ToString())
+                    new Claim(ClaimTypes.Role, user.UserType.ToString()) // will be validated against possible policies
                 },
-                signingCredentials: creds
+                signingCredentials: creds // add credentials so token can be validated upon later authentication
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
