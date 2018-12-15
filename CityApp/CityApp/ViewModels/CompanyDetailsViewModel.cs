622b8e5f5235a2fbb83e4fa4061ff1a546cf2918
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-12-15 22:32:28 +0100

    Add abonneer function

--- CityApp/CityApp/ViewModels/CompanyDetailsViewModel.cs
+++ CityApp/CityApp/ViewModels/CompanyDetailsViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CityApp.DataModel;
+using CityApp.Helpers;
 using CityApp.Services;
 using CityApp.Services.Navigation;
 using Windows.UI.Xaml.Navigation;
@@ -14,15 +15,23 @@ namespace CityApp.ViewModels
     public class CompanyDetailsViewModel : INavigableTo, INotifyPropertyChanged
     {
         public Company Company { get; private set; }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand SubscribeCommand { get; set; }
 
         private INavigationService _navigationService;
 
         public CompanyDetailsViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            SubscribeCommand = new RelayCommand((p) => AddCompanyToSubscription());
         }
 
+        private void AddCompanyToSubscription()
+        {
+            Console.WriteLine(Company);
+        }
 
         public async Task NavigatedTo(NavigationMode navigationMode, object parameter)
         {
--- CityApp/CityApp/Views/Company/CompanyDetails.xaml.cs
+++ CityApp/CityApp/Views/Company/CompanyDetails.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CityApp.Services.Navigation;
+﻿using CityApp.Services;
+using CityApp.Services.Navigation;
 using CityApp.ViewModels;
 using Windows.UI.Xaml.Controls;
 
@@ -7,11 +8,14 @@ namespace CityApp.Views
     public sealed partial class CompanyDetails : Page, IPageWithViewModel<CompanyDetailsViewModel>
     {
         public CompanyDetailsViewModel ViewModel { get; set; }
+        public bool CanSubscribe { get; set; }
 
         public CompanyDetails()
         {
             this.InitializeComponent();
+            ViewModel = new CompanyDetailsViewModel(NavigationService.ns);
+            this.DataContext = ViewModel;
+            CanSubscribe = StorageService.UserType == 1 ? true : false;
         }
-
     }
 }
