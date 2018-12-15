56e7fdf8b2916feb77b357bbedf2b2bd7ca12efe
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-12-16 00:22:32 +0100

    Update REST with subscription for user

--- CityAppREST/CityAppREST/Models/User.cs
+++ CityAppREST/CityAppREST/Models/User.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CityAppREST.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string Username { get; set; }
-        public DateTime BirthDate { get; set; }
-        public int Age => DateTime.Today.Year - BirthDate.Year;
-        public string Email { get; set; }
-        // Password should not be a String
-        public string Password { get; set; }
-        public IEnumerable<Company> Companies { get; set; }
-        public UserType UserType { get; set; }
+namespace CityAppREST.Models {
+	public class User {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string FirstName { get; set; }
+		public string Username { get; set; }
+		public DateTime BirthDate { get; set; }
+		public int Age => DateTime.Today.Year - BirthDate.Year;
+		public string Email { get; set; }
+		// Password should not be a String
+		public string Password { get; set; }
+		public IEnumerable<Company> Companies { get; set; }
+		public UserType UserType { get; set; }
+		public List<Company> Subscriptions { get; set; } = new List<Company>();
 
-        public User(string name, string firstName, string username, DateTime birthDate, String email, String password, IEnumerable<Company> companies, UserType userType)
-        {
-            Name = name;
-            FirstName = firstName;
-            Username = username;
-            BirthDate = birthDate;
-            Email = email;
-            Password = password;
-            UserType = userType;
-            Companies = companies;
-        }
+		public User(string name, string firstName, string username, DateTime birthDate, String email, String password, IEnumerable<Company> companies, UserType userType)
+		{
+			Name = name;
+			FirstName = firstName;
+			Username = username;
+			BirthDate = birthDate;
+			Email = email;
+			Password = password;
+			UserType = userType;
+			Companies = companies;
+		}
 
-        protected User()
-        {
-        }
-    }
+		protected User()
+		{
+		}
+	}
 }
